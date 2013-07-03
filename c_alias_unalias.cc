b087329dd418c8201f613a27fad10a45ae83d928
Rishabh Yadav <rishabh.ece.iitbhu@gmail.com>
2013-07-03 18:05:29 +0530

    added unaliascommand

--- c_alias_unalias.cc
+++ c_alias_unalias.cc
@@ -1,4 +1,4 @@
-/*$Id: c_alias.cc,v 26.133 2013/07/06 23:06:04 al Exp $ -*- C++ -*-
+/*$Id: c_testing.cc,v 26.133 2013/07/06 23:06:04 al Exp $ -*- C++ -*-
  * Copyright (C) 2013 Rishabh Yadav
  * Author: Rishabh Yadav <rishabh.ece.iitbhu@gmail.com>
  *
@@ -19,35 +19,53 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA.
  *------------------------------------------------------------------
- * Creates an alias for command.
+ * alias:creates an alias for a command
+ * unalias:removes the aliases for the given name
  */
 
 #include "c_comand.h"
 #include "globals.h"
+#include "alias.h"
+
 /*--------------------------------------------------------------------------*/
-
-class CMD_EXEC :public CMD {
-private:
-  std::string _command_string;
-public:
-  CMD_EXEC(std::string s)
-        : _command_string(s) {
-  }
-  void do_it(CS& Cmd, CARD_LIST* Scope) {
-    command(_command_string + Cmd.tail(), Scope);
-  }
-
-};
 
 class CMD_ALIAS : public CMD {
 public:
   void do_it(CS& Cmd, CARD_LIST* Scope) {
+    int i;
     std::string alias_name = Cmd.ctos();
     CMD_EXEC* new_command = new CMD_EXEC(Cmd.tail());
-    new DISPATCHER<CMD>::INSTALL(&command_dispatcher, alias_name, new_command); 
+    DISPATCHER<CMD>::INSTALL* install_ref = new DISPATCHER<CMD>::INSTALL(&command_dispatcher, alias_name, new_command); 
+    new_command->set_aliasname(alias_name,install_ref);
   }
 }alias_command;
 DISPATCHER<CMD>::INSTALL d(&command_dispatcher,"alias",&alias_command);
 
-/*-------------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------*/
+
+class CMD_UNALIAS : public CMD {
+private:
+     std::string alias_name;
+     int flag;
+public:
+     void do_it(CS& Cmd,CARD_LIST* Scope){
+        alias_name = Cmd.tail();
+        flag=1;
+        for(int i=0;objects_ptr[i]!='\0';i++)
+        {
+          if (objects_ptr[i]->get_aliasname() == alias_name){
+               objects_ptr[i]->uninstall();
+               flag=0;
+               break;
+            }
+         }  
+
+         if (flag){
+          IO::mstdout << "No such aliased name exists\n";
+         }  
+}
+}unalias_command;
+DISPATCHER<CMD>::INSTALL d1(&command_dispatcher,"unalias",&unalias_command);
+
+/*----------------------------------------------------------------------------*/
 
