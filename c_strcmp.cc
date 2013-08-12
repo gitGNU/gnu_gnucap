95e74f426150691b88424619af88f38f12029e82
Rishabh Yadav <rishabh.ece.iitbhu@gmail.com>
2013-08-13 01:49:16 +0530

    updated strcmp

--- c_strcmp.cc
+++ c_strcmp.cc
@@ -24,11 +24,13 @@
 
 #include <gnucap/c_comand.h>
 #include <gnucap/globals.h>
+#include <gnucap/u_parameter.h>
+#include <sstream>
 
 /*--------------------------------------------------------------------------*/
 class CMD_STRCMP : public CMD {
 public:
-  void do_it(CS& cmd, CARD_LIST*)
+  void do_it(CS& cmd, CARD_LIST*Scope)
   { 
     static int flag;
     std::string s1,s2;
@@ -46,6 +48,8 @@ public:
       
     }
     IO::mstdout<<"flag="<<flag<<"\n";
+    PARAM_LIST* pl = Scope->params();
+    pl->set("flag",static_cast<std::ostringstream*>( &(std::ostringstream() << flag) )->str());
 
 }
 }p;
