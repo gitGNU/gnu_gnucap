76d896c6afdd4eaaef306e3dc8f738fbb682bd6a
Rishabh Yadav <rishabh.ece.iitbhu@gmail.com>
2013-08-14 02:10:14 +0530

    updated showmod

---
+++ c_showmod.cc
@@ -0,0 +1,48 @@
+/*$Id: c_showmod.cc,v 26.130 2013/08/25 23:53:59 al Exp $ -*- C++ -*-
+ * Copyright (C) 2013 Rishabh Yadav
+ * Author: Rishabh Yadav <rishabh.ece.iitbhu@gmail.com>
+ *
+ * This file is part of "Gnucap", the Gnu Circuit Analysis Package
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *------------------------------------------------------------------
+ */
+
+#include "c_comand.h"
+#include "u_parameter.h"
+#include "globals.h"
+
+/*--------------------------------------------------------------------------*/
+namespace {
+/*--------------------------------------------------------------------------*/
+class CMD_SHOWMOD : public CMD {
+public:
+  void do_it(CS& cmd, CARD_LIST* Scope)
+  {
+    PARAM_LIST* pl = Scope->params();
+    if (cmd.is_end()) {
+      pl->print(IO::mstdout, OPT::language);
+      IO::mstdout << '\n';
+    }else{
+      pl->parse(cmd);
+    }
+  }
+} p;
+DISPATCHER<CMD>::INSTALL d(&command_dispatcher, "showmod", &p);
+/*--------------------------------------------------------------------------*/
+}
+/*--------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------*/
