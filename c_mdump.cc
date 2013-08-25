2daa0da9d402b58ada0f7adda66a7b7b87717e2a
Rishabh Yadav <rishabh.ece.iitbhu@gmail.com>
2013-08-26 00:32:42 +0530

    updated c_mdump

--- c_mdump.cc
+++ c_mdump.cc
@@ -22,57 +22,64 @@
  * mdump commands prints the dc-tran matrix into the console.if the filename is specified then output is redirected to the file.
  */
 
-#include <gnucap/c_comand.h>
-#include <gnucap/globals.h>
-#include <gnucap/u_sim_data.h>
+#include "c_comand.h"
+#include "globals.h"
+#include "u_sim_data.h"
 
+using namespace std;
 /*--------------------------------------------------------------------------*/
-class CMD_MDUMP : public CMD {
+class CMD_MDUMP: public CMD {
 public:
   void do_it(CS& cmd, CARD_LIST* )
   {
     std::string mode,filename;
     cmd >> filename;
     assert(_sim);
-    unsigned size_aa = _sim->_aa.size();
-    unsigned size_lu = _sim->_lu.size();
+    unsigned size_aa = _sim->_aa.size();//size of matrix before lu decomposition
+    unsigned size_lu = _sim->_lu.size();//size of matrix after lu decomposition
     if(!size_aa) throw Exception(".");
     IO::mstdout.setfloatwidth(4);
     if (filename!= ""){
       FILE *fptr;
       fptr = fopen(filename.c_str(),"a+");
-      if (!fptr) throw Exception("Unable to open or create file.");
+      if (!fptr) {fclose(fptr);throw Exception("Unable to open or create file.");}
       fprintf(fptr,"DC-Tran matrix before LU decomposition:\n");
       for(unsigned i=1;i<=size_aa;i++){
           for(unsigned j=1;j<=size_aa;j++)              
-                fprintf(fptr,"%8.6f \t",(_sim->_aa).s(i,j));
+                fprintf(fptr,"%8.4f \t",(_sim->_aa).s(i,j));
            fprintf(fptr,"\n");
        }
 
       fprintf(fptr,"DC-Tran matrix after LU decomposition:\n");
       for(unsigned i=1;i<=size_lu;i++){ 
            for(unsigned j=1;j<=size_lu;j++)
-                fprintf(fptr,"%8.6f \t",(_sim->_lu).s(i,j));
+                fprintf(fptr,"%8.4f \t",(_sim->_lu).s(i,j));
            fprintf(fptr,"\n");
        }       
-      
+      fclose(fptr);
      }
- 
     else{
        IO::mstdout << "DC-Tran matrix before LU decomposition:\n";
+       std::string io_s; //formatted string to be printed at console
        for(unsigned i=1;i<=size_aa;i++){
-             for(unsigned j=1;j<=size_aa;j++)              
-                IO::mstdout << (_sim->_aa).s(i,j)<<"\t\t\t";
+             for(unsigned j=1;j<=size_aa;j++) {     
+                io_s = to_string(_sim->_aa.s(i,j));
+                io_s.resize(10,' ');     
+                IO::mstdout << io_s <<"\t\t\t";
+                }
            IO::mstdout<<"\n";
        }
 
      IO::mstdout << "DC-Tran matrix after LU decomposition:\n";
      for(unsigned i=1;i<=size_lu;i++){ 
-           for(unsigned j=1;j<=size_lu;j++)
-                IO::mstdout << (_sim->_lu).s(i,j) <<"\t\t\t";
+           for(unsigned j=1;j<=size_lu;j++){
+               io_s = to_string(_sim->_lu.s(i,j));
+               io_s.resize(10,' ');
+               IO::mstdout << io_s<<"\t\t\t";
+               }
            IO::mstdout<<"\n";
-       }       
-    } 
+       }
+     }       
  }
 } p;
 DISPATCHER<CMD>::INSTALL d3(&command_dispatcher, "mdump", &p);
