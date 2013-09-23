4923d35b5cd86d500a93c8e0996ae0d5cc858ea6
Felix Salfelder <felix@em.cs.uni-frankfurt.de>
2013-09-23 15:09:49 +0200

    filter update

--- tests/filter.c
+++ tests/filter.c
@@ -10,18 +10,32 @@ int main(int argc, char * const * argv){
 
 	char data = getchar();
 	int s = 0;
+	char end = ' ';
 	while( data != EOF ){
-		char c = fgetc(template);
-		if(s && c==' ' && '0' <= data && data <= '9'){
-			data=' ';
-		}else if(c=='~' && '0' <= data && data <= '9'){
+		char t = fgetc(template);
+		if(t==EOF) break;
+		if(s && t==' ' && '0' <= data && data <= '9'){
+			data=' '; end=' ';
+		}else if(s && t==end){
+			data = end;
+			end=' '; s=0;
+//		}else if(s && data=='n'){
+//			end=' '; s=0;
+		}else if(s && ( t=='n' || t=='f' || t=='p' || t=='u' ) && '0' <= data && data <= '9'){
+			data = t;
+			end=' ';
+			s=1;
+		}else if(t=='~' && '0' <= data && data <= '9'){
 			data='~';
 			s=1;
+		}else if(t=='~' && ( 'u' == data || 'n' == data || 'f' == data || 'p' == data )){
+			end = data;
+			data = '~';
+			s = 1;
 		}else{
-			s=0;
+			s = 0;
 		}
 		putchar(data);
-		if(c==EOF) break;
 	   data = getchar();
 	}
 	fclose(template);
