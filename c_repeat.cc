82c3869494bd6921d07858a4ab260f246b03a01d
Rishabh Yadav <rishabh.ece.iitbhu@gmail.com>
2014-08-01 03:48:59 +0530

    repeat plugin:code update

--- c_repeat.cc
+++ c_repeat.cc
@@ -22,7 +22,8 @@
  
 #include <gnucap/c_comand.h>
 #include <gnucap/globals.h>
-#include <gnucap/d_dot.h>
+//Modified d_dot.h to make the clone member function from DEV_DOT class a public member function.
+#include "d_dot.h"
 #include <gnucap/e_cardlist.h>
 #include <gnucap/ap.h>
 #include <stdlib.h>
@@ -33,47 +34,48 @@ namespace {
 class CMD_REPEAT : public CMD {
 public:
   void do_it(CS& cmd, CARD_LIST* Scope) {
-				//Get the number of ties loop has to be repeated.
+				//Get the number of times loop has to be repeated.
 			 int counter = atoi((cmd.ctos()).c_str());
-			 IO::mstdout << "Counter" << counter << "\n";
+			 
+			 //std::cout << "Counter:" << counter << "\n";
 			 
 			 //Create a CARD_LIST object to store the body of the loop.
-			 CARD_LIST* new_module = subckt();
+			 CARD_LIST new_module;;
 			 
 			 //Read the body
-			 while(!(cmd >> "end ")){
+			  do{
 				 //Create a DEV_DOT object to store the command as a string.
-				 DEV_DOT* _command = new DEV_DOT();
+				 DEV_DOT* instruction = new DEV_DOT();
 				 				 
 				 //Get the command.
 				 cmd.get_line(">");
 				 
 				 //Store the command.
-				 _command->set(cmd.fullstring());
+				 instruction->set(cmd.fullstring());
 				 
-				 //Insert the DEV_DOT object into the list.
-				 new_module->push_front(_command);
-			}
+				 //Insert the cloned DEV_DOT object into the list.
+				 new_module.push_back(instruction->clone());
+			}while(!(cmd >> "end "));
 			
 			//Execute the body
-			typedef std::list<CARD*>::iterator iterator;
-			if (!new_module->is_empty()){	
-				while(counter!=0){
-				   for (iterator ci=new_module->begin(); ci!=new_module->end(); ++ci) {
-								//Retrieve the object at index ci;								
-								std::string ptr_command = (dynamic_cast<DEV_DOT*>(*ci))->s();
-								
-								//ISSUE:Retrieved object is CARD* not DEV_DOT and since the CARD* don't have access to  DEV_DOT::s() function so command string is not retrieveable. 
+			
+			if (!new_module.is_empty()){	
+				while(counter>0){std::cout << "in while\n";
+				   for(CARD_LIST::iterator i=new_module.begin(); i!=new_module.end(); ++i){
+								DEV_DOT* ptr_command = dynamic_cast<DEV_DOT*>(*i);
+								assert(ptr_command);
+								//ISSUE:Retrieved object is CARD* not DEV_DOT and since the CARD* don't have access to DEV_DOT::s()
+								//function so command string is not retrievable. 
 								
 								//Test if the order of command execution is correct.
-								IO::mstdout << "command: " << ptr_command << "\n";
+								std::cout << "command string is " << ptr_command->s() << "\n";	
 								
-								//Construct the CS object with command as a parameter.
+								//Construct the CS object with ptr_command as a parameter.
 								/*
 								CS cmd_str = CS(CS::_STRING,ptr_command->s());
 								cmdproc(cmd_str,Scope);
 								*/
-								command(ptr_command,Scope);
+								command(ptr_command->s(),Scope);
 				}
 				counter--;
 			}
