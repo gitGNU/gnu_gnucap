ac63db6cd139ffd100f3869c19c447fff309744a
Rishabh Yadav <rishabh.ece.iitbhu@gmail.com>
2014-08-02 00:20:25 +0530

    repeat command -v1

--- c_repeat.cc
+++ c_repeat.cc
@@ -22,8 +22,8 @@
  
 #include <gnucap/c_comand.h>
 #include <gnucap/globals.h>
-//Modified d_dot.h to make the clone member function from DEV_DOT class a public member function.
-#include "d_dot.h"
+//Modified the privacy of clone() member function and copy constructor in d_dot.h
+#include <gnucap/d_dot.h>
 #include <gnucap/e_cardlist.h>
 #include <gnucap/ap.h>
 #include <stdlib.h>
@@ -34,48 +34,45 @@ namespace {
 class CMD_REPEAT : public CMD {
 public:
   void do_it(CS& cmd, CARD_LIST* Scope) {
-				//Get the number of times loop has to be repeated.
-			 int counter = atoi((cmd.ctos()).c_str());
+		//Get the number of times loop has to be repeated.
+		int counter = atoi((cmd.ctos()).c_str());
+
+		//Create a CARD_LIST object to store the body of the loop.
+		CARD_LIST new_module;;
 			 
-			 //std::cout << "Counter:" << counter << "\n";
-			 
-			 //Create a CARD_LIST object to store the body of the loop.
-			 CARD_LIST new_module;;
-			 
-			 //Read the body
-			  do{
-				 //Create a DEV_DOT object to store the command as a string.
-				 DEV_DOT* instruction = new DEV_DOT();
-				 				 
-				 //Get the command.
-				 cmd.get_line(">");
+		//Read the body
+		cmd.get_line(">");
+		while(!(cmd >> "end ")){untested();
+			 //Create a DEV_DOT object to store the command as a string.
+			DEV_DOT* instruction = new DEV_DOT();	
 				 
-				 //Store the command.
-				 instruction->set(cmd.fullstring());
-				 
-				 //Insert the cloned DEV_DOT object into the list.
-				 new_module.push_back(instruction->clone());
-			}while(!(cmd >> "end "));
+			//Store the command.
+			instruction->set(cmd.fullstring());
 			
-			//Execute the body
+			//TODO::DEV_DOT clone() not working so directly calling the constructor for the time being.
+			CARD* ptr_comand = new DEV_DOT(*instruction);	 
 			
-			if (!new_module.is_empty()){	
-				while(counter>0){std::cout << "in while\n";
-				   for(CARD_LIST::iterator i=new_module.begin(); i!=new_module.end(); ++i){
-								DEV_DOT* ptr_command = dynamic_cast<DEV_DOT*>(*i);
-								assert(ptr_command);
-								//ISSUE:Retrieved object is CARD* not DEV_DOT and since the CARD* don't have access to DEV_DOT::s()
-								//function so command string is not retrievable. 
-								
-								//Test if the order of command execution is correct.
-								std::cout << "command string is " << ptr_command->s() << "\n";	
-								
-								//Construct the CS object with ptr_command as a parameter.
-								/*
-								CS cmd_str = CS(CS::_STRING,ptr_command->s());
-								cmdproc(cmd_str,Scope);
-								*/
-								command(ptr_command->s(),Scope);
+			//Insert the cloned DEV_DOT object into the list.
+			new_module.push_back(ptr_comand);
+			
+			//Get the command.
+			cmd.get_line(">");
+
+		}
+			
+		//Execute the body of the loop			
+		if (!new_module.is_empty()){untested();
+			while(counter>0){itested();
+				for(CARD_LIST::iterator i=new_module.begin(); i!=new_module.end(); ++i){untested();
+					DEV_DOT* ptr_command = dynamic_cast<DEV_DOT*>(*i);
+					assert(ptr_command);
+													
+					//Construct the CS object with ptr_command as a parameter.
+					/*
+					CS cmd_str = CS(CS::_STRING,ptr_command->s());
+					cmdproc(cmd_str,Scope);
+					*/
+					command(ptr_command->s(),Scope);
 				}
 				counter--;
 			}
