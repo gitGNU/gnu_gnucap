3223ff6cabfa49fd9cbe9944bac9e12d1820acf7
Felix Salfelder <felix@salfelder.org>
2016-02-12 08:42:47 +0000

    introduce version triplets for package and library

--- include/patchlev.h
+++ include/patchlev.h
@@ -1,1 +1,14 @@
-#define PATCHLEVEL "master 2016-02-02a"
+// package version
+#define PACKAGE_VERSION 0
+#define PACKAGE_REVISION 31
+// #define PACKAGE_COMMIT 0 // distance to the last git tag (not yet)
+
+// library version, see
+// https://www.gnu.org/software/libtool/manual/html_node/Updating-version-info.html
+#define LIBGNUCAP_CURRENT 0
+#define LIBGNUCAP_REVISION 0 // or anything really close...
+#define LIBGNUCAP_CURRENT 0
+
+// custom string for the banner
+#define PATCHLEVEL "version_triplets 2016-02-12"
+
