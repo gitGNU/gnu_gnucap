ac5d9929e6a96703f031fa9a308a10f91b340b6f
Felix Salfelder <felix@salfelder.org>
2016-11-13 15:20:49 +0000

    return to ichar_traits (needs debugging..)
    need a different word order. not the one induced by the character order.
    
    consider aA aB Ac in the dictionary. that must be the ascending order in
    insensitive mode. in sensitive mode, we may want to add AB. that must go
    between aA and aB, so it can be located in both sensitive and
    insensitive mode...

--- include/l_istring.h
+++ include/l_istring.h
@@ -37,21 +37,23 @@ struct Ichar{
   Ichar() : _c('\0') {untested();}
   Ichar(const Ichar& c) : _c(c._c) {untested();}
   explicit Ichar(char c) : _c(c) {untested();}
-  bool operator==(char o) const { untested();
-    if(OPT::case_insensitive){
+  bool operator==(char o) const {
+    if(OPT::case_insensitive){ untested();
       return tolower(_c)==tolower(o);
-    }else{
-      return o == _c;
+    }else if(o==_c){ untested();
+      return true;
+    }else{ untested();
+      return false;
     }
   }
-  bool operator!=(char o) const { untested();
-    return(!operator==(o));
-  }
+ // bool operator!=(char o) const { untested();
+ //   return(!operator==(o));
+ // }
   bool operator==(Ichar o) const { untested();
-    if(OPT::case_insensitive){
+    if(OPT::case_insensitive){ untested();
       return tolower(_c)==tolower(o._c);
-    }else{
-      return o == _c;
+    }else{ untested();
+      return o._c == _c;
     }
   }
   bool operator!=(Ichar o) const { untested();
@@ -68,14 +70,84 @@ struct Ichar{
   bool operator!() const{ untested();
     return !_c;
   }
-  char const& to_char() const{untested(); return _c;}
+  operator bool() const{ untested();
+    return _c;
+  }
+  char const& to_char() const{itested(); return _c;}
+  char to_lower() const{untested(); return (char)tolower(_c);}
 private:
   char _c;
 };
 /*--------------------------------------------------------------------------*/
-class IString : public std::basic_string<Ichar> { //
+namespace detail{
+struct ichar_traits : std::char_traits<Ichar>{
+  typedef char_traits<char> base;
+  typedef Ichar char_type;
+  typedef int int_type;
+  typedef std::streamoff off_type;
+  typedef std::streampos pos_type;
+  // inherited
+  // static bool lt(const Ichar& a, const Ichar& b);
+  // static bool eq(const Ichar& a, const Ichar& b);
+  // static void copy(Ichar* x, const Ichar* y, size_t s);
+  // static size_t length(const Ichar* x);
+
+  // compare needs to be different. default to insensitive order.
+  // if enabled, use sensitive order as a tie break.
+  static int compare (const char_type* p, const char_type* q, size_t)
+  { untested();
+    typedef enum{
+      lt  =-1,
+      same=0,
+      gt  =1
+    }ord_t;
+    char_type const* i=p;
+    char_type const* j=q;
+
+    ord_t ord=same;
+    for(; *i; ++i, ++j){ untested();
+      if(*j=='\0'){ untested();
+	// a is a proper prefix of b
+	ord = gt;
+	trace3("done1", (char const*)(p), (char const*)(q), ord);
+	return ord;
+      }else if(i->to_char()==j->to_char()){ untested();
+      }else if(i->to_lower() < j->to_lower()){ untested();
+	ord = lt;
+	trace3("done2", (char const*)(p), (char const*)(q), ord);
+	return ord;
+      }else if(j->to_lower() < i->to_lower()){ untested();
+	ord = gt;
+	trace3("done2", (char const*)(p), (char const*)(q), ord);
+	return ord;
+      }else if(OPT::case_insensitive){ untested();
+      }else if(ord!=same){ untested();
+      }else if(i->to_char() == j->to_lower()){ untested();
+	ord = gt; // lower case before upper case...
+      }else if(i->to_lower() == j->to_char()){ untested();
+	ord = lt;
+      }else{ untested();
+      }
+    }
+    // b is a prefix of a...
+    if(OPT::case_insensitive){ untested();
+      ord = (*j=='\0')? same: gt;
+    }if(*j=='\0'){ untested();
+      // same length. order aAAaa before aaaaa
+      trace3("samelen sensitive", (char const*)(p), (char const*)(q), ord);
+      // return ord;
+    }else{ untested();
+      ord = lt;
+    }
+    trace3("done", (char const*)(p), (char const*)(q), ord);
+    return ord;
+  }
+};
+}
+/*--------------------------------------------------------------------------*/
+class IString : public std::basic_string<Ichar, detail::ichar_traits> { //
 private:
-  typedef std::basic_string<Ichar> base;
+  typedef std::basic_string<Ichar, detail::ichar_traits> base;
 public:
   IString() { untested(); }
   IString(const IString& s) : base(s) { untested(); }
@@ -92,9 +164,24 @@ public: // ops
     base::operator=((Ichar const*)s);
     return *this;
   }
+  bool operator==(const IString& s) const{ untested();
+    return base(*this)==base(s);
+  }
   IString& operator+=(const std::string& s){ untested();
     base::operator+=(IString(s));
     return *this;
+  }
+  bool operator==(char c) const { untested();
+    return size()==1 && base::operator[](0).to_char()==c;
+  }
+  bool operator!=(char c) const { untested();
+    return !(*this==c);
+  }
+  bool operator==(char const* c) const { untested();
+    return (*this==IString(c));
+  }
+  bool operator!=(char const* c) const { untested();
+    return !(operator==(c));
   }
 public: // more conventional type bridge
   size_type find(char x, size_type y) const { untested();
@@ -111,31 +198,14 @@ public: // more conventional type bridge
     return reinterpret_cast<std::string const&>(*this);
   }
 public: // implicit conversion
+ // operator bool() const { untested();
+ //   return size();
+ // }
 //   operator const std::string&() const
 //   { untested();
 //     return reinterpret_cast<std::string const&>(*this);
 //   }
-};
-/*--------------------------------------------------------------------------*/
-inline bool operator==(const IString& s, char c)
-{ untested();
-  return s.size()==1 && s[0]==c;
-}
-/*--------------------------------------------------------------------------*/
-inline bool operator!=(const IString& s, char c)
-{ untested();
-  return !(s==c);
-}
-/*--------------------------------------------------------------------------*/
-inline bool operator==(const IString& s, const char* c)
-{ untested();
-  return s == IString(c);
-}
-/*--------------------------------------------------------------------------*/
-inline bool operator!=(const IString& s, const char* c)
-{ untested();
-  return !(s==c);
-}
+}; // IString
 /*--------------------------------------------------------------------------*/
 inline std::string operator+(IString s, char x)
 { untested();
